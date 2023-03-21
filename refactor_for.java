ae508ba84a2ff59cf1f2c966f92aa47581ac2e4c
tigre437 <davidserranojuegos@gmail.com>
2023-03-21 09:29:26 +0100

    fix: quitado un comentario de refactor_for.java que no era necesario

--- refactor_for.java
+++ refactor_for.java
@@ -1,7 +1,7 @@
 public class refactor_for {
 
     public void order(String[] books) {
-        /**/for (int i = 0; i < books.length; i++) {  //convertir el for a for mejorado
+        for (int i = 0; i < books.length; i++) { // convertir el for a for mejorado
             // do something
         }
     }
